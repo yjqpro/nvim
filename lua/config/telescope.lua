ae295cf0fe33c495d334cc662eeaef8b58713fd6
yjqpro <yjqpro@yjqprodeMacBook-Pro.local>
2021-10-19 06:27:35 +0800

    feat (file finder): update config for mappings

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,6 +3,9 @@ require('telescope').setup{
 	defaults  = {
 		mappings  = {
 			i = {
+        ["<C-n>"] = false,
+        ["<C-p>"] = false,
+        ["<C-[>"] = actions.close,
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
 				["<C-]>"] = actions.file_vsplit
