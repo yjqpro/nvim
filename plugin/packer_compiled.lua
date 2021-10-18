167f9bc346858bcf47a30aab57a14af0c2ecac90
unknown <yjqpro@gmail.com>
2021-10-18 09:27:20 +0800

    docs : add README.md

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/yjqpro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/yjqpro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/yjqpro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/yjqpro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/yjqpro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "C:\\Users\\yjqpr\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?.lua;C:\\Users\\yjqpr\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\yjqpr\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\yjqpr\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
+local install_cpath_pattern = "C:\\Users\\yjqpr\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\lua\\5.1\\?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,35 +76,35 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/asyncrun.vim"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\asyncrun.vim"
   },
   ["asynctasks.vim"] = {
     after = { "asyncrun.vim" },
     commands = { "AsyncRun", "AsyncStop", "AsyncTask", "AsyncTaskEdit" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/asynctasks.vim"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\asynctasks.vim"
   },
   ["gruvbox-material"] = {
     config = { '\t\tvim.cmd("colorscheme gruvbox")\n\t\t' },
     loaded = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/start/gruvbox-material"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gruvbox-material"
   },
   ["nvim-autopairs"] = {
     config = { "\t\trequire('nvim-autopairs').setup{}\n\t\t" },
     loaded = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/start/nvim-autopairs"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-autopairs"
   },
   ["nvim-lspconfig"] = {
     config = { "      require('config.nvim-lspconfig')\n    " },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/nvim-lspconfig"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\nvim-lspconfig"
   },
   ["plenary.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/plenary.nvim"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\plenary.nvim"
   },
   ["sqlite.lua"] = {
     load_after = {
@@ -112,53 +112,53 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/sqlite.lua"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\sqlite.lua"
   },
   ["telescope-frecency.nvim"] = {
     after = { "sqlite.lua" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/telescope-frecency.nvim",
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\telescope-frecency.nvim",
     wants = { "sqlite.lua" }
   },
   ["telescope-fzf-native.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/telescope-fzf-native.nvim"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\telescope-fzf-native.nvim"
   },
   ["telescope.nvim"] = {
     commands = { "Telescope" },
     config = { "\t\t  require('config.telescope')\n\t\t" },
     loaded = false,
     needs_bufread = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/telescope.nvim",
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\telescope.nvim",
     wants = { "plenary.nvim", "telescope-fzf-native.nvim", "telescope-frecency.nvim" }
   },
   ["vim-abolish"] = {
     loaded = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/start/vim-abolish"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-abolish"
   },
   ["vim-clang-format"] = {
     config = { "      vim.g['clang_format#auto_format'] = 1\n      vim.g['clang_format#code_style'] = 'chromium'\n      vim.g['clang_format#style_options'] = {SortIncludes = 'false'}\n    " },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-clang-format"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-clang-format"
   },
   ["vim-fugitive"] = {
     commands = { "Git", "G" },
     loaded = false,
     needs_bufread = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-fugitive"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-fugitive"
   },
   ["vim-rooter"] = {
     config = { "      vim.g.rooter_silent_chdir = 1\n      vim.g.rooter_patterns = {'.git', '.root'}\n\t\t" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-rooter"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-rooter"
   },
   ["vim-surround"] = {
     loaded = true,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/start/vim-surround"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-surround"
   },
   ["vim-textobj-parameter"] = {
     load_after = {
@@ -166,18 +166,18 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-textobj-parameter"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-textobj-parameter"
   },
   ["vim-textobj-user"] = {
     after = { "vim-textobj-parameter" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-textobj-user"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-textobj-user"
   },
   ["vim-unimpaired"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/yjqpro/.local/share/nvim/site/pack/packer/opt/vim-unimpaired"
+    path = "C:\\Users\\yjqpr\\AppData\\Local\\nvim-data\\site\\pack\\packer\\opt\\vim-unimpaired"
   }
 }
 
@@ -195,13 +195,13 @@ time([[Config for nvim-autopairs]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncTask lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncTask", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncTaskEdit lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncTaskEdit", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncRun lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncRun", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncStop lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncStop", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Git lua require("packer.load")({'vim-fugitive'}, { cmd = "Git", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file G lua require("packer.load")({'vim-fugitive'}, { cmd = "G", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Telescope lua require("packer.load")({'telescope.nvim'}, { cmd = "Telescope", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncTaskEdit lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncTaskEdit", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncTask lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncTask", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file AsyncRun lua require("packer.load")({'asynctasks.vim'}, { cmd = "AsyncRun", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 time([[Defining lazy-load commands]], false)
 
 vim.cmd [[augroup packer_load_aucmds]]
